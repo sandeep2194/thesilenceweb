1ea44bf877db4ef383c01f794d23bfd058c0f9a5
sandeep2194 <sandeep2194@live.com>
2021-06-10 21:35:09 +0530

    added logrocket

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,9 +2,11 @@ import { applyMiddleware } from 'redux'
 import logger from './logger'
 import thunk from 'redux-thunk'
 import authCheck from './authChecker'
+import LogRocket from 'logrocket';
 
 export default applyMiddleware(
     thunk,
     authCheck,
     logger,
+    LogRocket.reduxMiddleware(),
 )

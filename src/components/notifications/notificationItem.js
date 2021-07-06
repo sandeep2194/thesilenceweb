301cafe190bff9f03a1f219a27c77ba50b8950cf
sandeep2194 <sandeep2194@live.com>
2021-07-06 18:34:43 +0530

    fixed meta bugs

--- src/components/notifications/notificationItem.js
+++ src/components/notifications/notificationItem.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import moment from 'moment';
 
 class NotificationItem extends Component {
     render() {
@@ -9,7 +10,21 @@ class NotificationItem extends Component {
                 <Container>
                     <Row>
                         <Col lg={6}>
-
+                            <Row>
+                                <Col sm={2} xs={1}>
+                                    <Image src={authorImage} className="rounded-circle" alt={authorName}
+                                        height={32} width={32}
+                                    />
+                                </Col>
+                                <Col>
+                                    <Row>
+                                        <h5>{authorName + ' ' + message}</h5>
+                                    </Row>
+                                    <Row>
+                                        <span className='meta'>{moment.unix(pubDate).format("DD/MM/YYYY")}</span>
+                                    </Row>
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </Container>

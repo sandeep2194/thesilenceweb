d637b4aafaff13235bd6922558da01274fe2c0a2
sandeep2194 <sandeep2194@live.com>
2021-10-02 23:24:03 +0530

    fixed check error2

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,8 +1,9 @@
 import { toastr } from 'react-redux-toastr'
-
+import history from '../utils/history'
 export const CheckError = (e) => {
     const token = localStorage.getItem('token')
-    if (e.message.includes('403') && token !== undefined) {
+    if (e.message.includes('403') && token) {
+        history.push('/send-otp')
         toastr.info('please login again - session expired!')
     }
 }

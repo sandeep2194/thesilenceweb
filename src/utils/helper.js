3f962d564fcd7939d1151e336332fe757bbc8e44
sandeep2194 <sandeep2194@live.com>
2021-10-02 23:22:49 +0530

    token truthy

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,9 +1,11 @@
 
 import { toastr } from 'react-redux-toastr'
+import history from '../utils/history'
 
 export const CheckError = (e) => {
     const token = localStorage.getItem('token')
-    if (e.message.includes('403') && token === 'undefined') {
+    if (e.message.includes('403') && token) {
+        history.push('/send-otp')
         toastr.info('please login again - session expired!')
     }
 }
